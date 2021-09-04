415f4dd8dcf83a6b5ac3e1c158463e4160aab868
ricardomj0499 <ricardomj0499@hotmail.com>
2021-09-03 22:14:08 -0600

    [+-] arreglos y eliminaciones de archivos

--- src/smtpserver.py
+++ src/smtpserver.py
@@ -11,13 +11,18 @@ from zope.interface import implementer #
 from email.header import Header
 
 # Documentación: https://twistedmatrix.com/documents/current/api/twisted.mail.html
+# SMTP usualmente se usa solamente para el envío de correo
+# de recibir usualmente se encarga POP3 o IMAP
 
 '''
-Clase 
+Acá se implementa una interface para recibir emails
 '''
 @implementer(smtp.IMessage)
 class MaildirMessageWriter(object):
 
+    '''
+    Inicializador de la clase, Recibe el nombre del lugar a donde se guardarán los mensajes
+    '''
     def __init__(self, userDir):
         print("Init MaildirMessageWriter, función")
         if(not os.path.exists(userDir)):
@@ -26,12 +31,14 @@ class MaildirMessageWriter(object):
 
         inboxDir = os.path.join(userDir, 'Inbox')
         print("inbox dear: ", inboxDir)
-        self.mailbox = maildir.MaildirMailbox(inboxDir)
+        self.mailbox = maildir.MaildirMailbox(inboxDir) # Creará el inbox automaticamente
 
         self.lines = []
 
 
-
+    '''
+    Función llamada por cada linea del mensaje entrante
+    '''
     def lineReceived(self, line):
         print("linea recibida 2 función")
         if type(line) != str:
@@ -39,7 +46,9 @@ class MaildirMessageWriter(object):
         self.lines.append(line)
 
 
-
+    '''
+    Una vez todas el mensaje se lee complemante se llama
+    '''
     def eomReceived(self):
 
         # message is complete, store it
@@ -51,17 +60,20 @@ class MaildirMessageWriter(object):
         messageData = '\n'.join(self.lines)
         print("messageData: ", messageData)
         return self.mailbox.appendMessage(bytes(messageData, "UTF-8"))
+    
 
-
-
+    '''
+    Usada en caso de que la energía se vaya
+    '''
     def connectionLost(self):
 
         print("Connection lost unexpectedly!")
-
-        # unexpected loss of connection; don't save
+        # Sí se pierde la conexión no guarfa las lineas
         del(self.lines)
 
+'''
 
+'''
 @implementer(smtp.IMessageDelivery)
 class LocalDelivery(object):
 
@@ -78,24 +90,33 @@ class LocalDelivery(object):
         self.validDomains = validDomains
 
 
-
+    '''
+    genera un encabezado "received" que será agregado al al email entrante
+    el smtp es el encargado de agregar este encabezado a los mensajes entrantes
+    helo: (server name al que el ciente se dirigió, client IP Adress)
+    origin: es un smtp.addres que identifica de quien viene el mensaje
+    recipients: lista de smtp.addres de a quien va el mensaje
+    '''
     def receivedHeader(self, helo, origin, recipients):
         print("entre a receivedHeader")
         myHostname, clientIP = helo
 
-        headerValue = "by %s from %s with ESMTP ; %s" % (
-
-        myHostname.decode(), clientIP.decode(), smtp.rfc822date().decode())
+        headerValue = "by %s from %s with ESMTP ; %s" % (myHostname.decode(), clientIP.decode(), smtp.rfc822date().decode())
 
         # email.Header.Header used for automatic wrapping of long lines
 
         return "Received: %s" % Header(headerValue)
 
-
-
+    '''
+    acepta o rechaza los emails dependiendo de quien vienen
+    '''
+    '''
+    User contiene información de la dirrección del recipiente e info desde donde viene el mensaje
+    '''
     def validateTo(self, user):
 
         print("valida to to: ", user)
+    
         if not user.dest.domain.decode("UTF-8") in self.validDomains:
 
             raise smtp.SMTPBadRcpt(user)
@@ -111,11 +132,18 @@ class LocalDelivery(object):
         return os.path.join(self.baseDir, "%s" % address)
 
 
-
+    '''
+    validateFrom takes two arguments: a tuple with the hostname used in by the client when it said HELO and the client's IP address
+    tupla (hostname usado por el cliente cuando dice "helo", y la dirección IP del cliente)
+    y un smtp.adrres que identifica el enviador(sender)
+    '''
     def validateFrom(self, helo, originAddress):
         print("validato from ", originAddress)
         # accept mail from anywhere. To reject an address, raise
-
+        if not helo:
+            raise smtp.SMTPBadSender(originAddress, 503, "Who are you?  Say HELO first.")
+        if not originAddress.domain:
+            raise smtp.SMTPBadSender(originAddress, 501, "Sender address must contain domain.")
         # smtp.SMTPBadSender here.
         return originAddress
 
@@ -140,7 +168,8 @@ class SMTPFactory(protocol.ServerFactory):
     def buildProtocol(self, addr):
         print("Building protocol")
         delivery = LocalDelivery(self.baseDir, self.validDomains)
-
+        print("delivery++++: ", delivery.baseDir)
+        print("delivery++++: ", delivery.validDomains)
         smtpProtocol = smtp.SMTP(delivery)
 
         smtpProtocol.factory = self
@@ -149,18 +178,27 @@ class SMTPFactory(protocol.ServerFactory):
 
 
 if __name__ == "__main__":
-
     print("He entrado al name==main")
-
     import sys
+    d = s = p = 0
+    domains = mailboxDir =  port = ''
 
-    mailboxDir = sys.argv[1]
-    print("mailboxDir es: ", mailboxDir)
+    try:
+        d = sys.argv.index('-d')
+        s = sys.argv.index('-s')
+        p = sys.argv.index('-p')
 
-    domains = sys.argv[2].split(",")
-    print("domains es: ", domains)
+        domains = (sys.argv)[d+1].split(',') # Dominios que acepta
+        print("domains es: ", domains)
+        mailboxDir = sys.argv[s+1] # Lugar donde se guardarán los correos
+        print("mailboxDir es:", mailboxDir)
+        port = sys.argv[p+1] # Puerto al que escuchará el server
+        print("port es:",port)
+    except NameError as e:
+        print(e)
+    
 
-    reactor.listenTCP(1234, SMTPFactory(mailboxDir, domains))   
+    reactor.listenTCP(int(port), SMTPFactory(mailboxDir, domains))   
 
     from twisted.internet import ssl
 
